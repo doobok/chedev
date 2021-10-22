76fcbc4e1f6a2979765558656d8968d1814909be
yura <yura.chernobay@gmail.com>
2021-10-22 13:42:03 +0300

    Init project

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="text-white body-font bg-gray-700 from-hero-gradient-from to-hero-gradient-to">
+  <div>
+    <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+      <p class="text-gray-300 text-sm text-center sm:text-left">© 2020 - {{date("Y")}} Copyright —
+        {{config('app.name')}}
+      </p>
+      <a href="/policy" class="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-300 text-sm">Политика конфиденциальности</a>
+    </div>
+  </div>
+</footer>
+<div hidden class="bg-primary-500 bg-primary-600 bg-secondary-500 bg-secondary-600"></div>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
+        './resources/js/**/*.vue',
+    ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        // primary: colors.green,
+        // secondary: colors.yellow,
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
