77113c3291b339f475023393ac4a42bcd26397fc
yura <yura.chernobay@gmail.com>
2022-04-17 12:49:26 +0300

    Add some global var

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -31,15 +31,15 @@
             </p>
         </div>
         <div class="mb-4 sm:text-center">
-            <a href="https://t.me/Yura_Chernobay" aria-label="Founder" class="inline-block mb-1" target="_blank">
+            <a href="{{ setting('contact.telegram_profile') }}" aria-label="{{ setting('contact.telegram_role_name') }}" class="inline-block mb-1" target="_blank">
                 <img alt="avatar"
                      src="/founder_photo.jpeg"
                      class="object-cover w-10 h-10 rounded-full shadow-sm"/>
             </a>
             <div>
-                <a href="https://t.me/Yura_Chernobay" aria-label="Founder" target="_blank"
-                   class="font-semibold text-gray-800 transition-colors duration-200 hover:text-green-700">Yuri Chernobay</a>
-                <p class="text-sm font-medium leading-4 text-gray-600">Founder</p>
+                <a href="{{ setting('contact.telegram_profile') }}" aria-label="{{ setting('contact.telegram_role_name') }}" target="_blank"
+                   class="font-semibold text-gray-800 transition-colors duration-200 hover:text-green-700">{{ setting('contact.telegram_username') }}</a>
+                <p class="text-sm font-medium leading-4 text-gray-600">{{ setting('contact.telegram_role_name') }}</p>
             </div>
         </div>
 
